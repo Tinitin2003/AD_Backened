c6b77386ae4d883f334273fabd17f5b30b644f5f
Tinitin2003 <nitinpanwar2828@gmail.com>
2023-01-09 22:43:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express=require("express");
+const mysql=require("mysql");
+const cors=require("cors");
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+const con= mysql.createConnection({
+    host: "sql6.freesqldatabase.com",
+    user: "sql6588358",
+    password: "btB6GzPkwa",
+    database:"sql6588358"
+})
+app.post('/register',(req,res)=>{
+    const username=req.body.username;
+    console.log(username);
+    const email=req.body.email;
+    console.log(email);
+    const password=req.body.password;
+    console.log(password);
+    console.log("regiter");
+    con.query("insert into AD_User_Detail (Name,Email,Password) values(?,?,?)",[username,email,password],
+        (err,result)=>{
+            if(result){
+                res.send(result);
+            }else{
+                res.send({message: "Enter Incorrect"})
+            }
+        }
+    )
+})
+app.post('/login',(req,res)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+    con.query("select * from AD_User_Detail where Email=? and Password=?",[email,password],
+        (err,result)=>{
+            if(err){
+                req.setEncoding({err:err});
+            }else{
+             if(result.length>0){
+                res.send(result);
+             }
+             else{
+                res.send({message: "Wrong username or password"});
+             }
+            }
+        }
+    )
+})
+app.listen(3001,()=>{
+    console.log("server is running");
+})
