21d348edba0b034a26a1bf65474c9a942d1eab01
Tinitin2003 <nitinpanwar2828@gmail.com>
2023-01-12 17:11:21 +0530

    more-specified

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app=express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://anantdarshan.netlify.app/",
+    origin: "https://anantdarshan.netlify.app",
 }));
 const con= mysql.createConnection({
     host: "sql6.freesqldatabase.com",
